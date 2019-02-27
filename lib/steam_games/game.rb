315808a5cfb00f2610fdf76c6ec32fa47bea00e5
Scott Silverman <scottms724@gmail.com>
2019-02-27 20:59:29 +0000

    WIP: Scraping Steam's website.

--- lib/steam_games/game.rb
+++ lib/steam_games/game.rb
@@ -8,15 +8,29 @@ class SteamGames::Game
    end 
   
    def self.popular_new_releases
+     self.scrape_games
+  
+   # game_1 = self.new("Skyrim", "96% positive out of 820 reviews.", "steam.com/skyrim/popularnewreleases")
+   # game_2 = self.new("Overwatch", "98% positive out of 1,162 reviews.", "steam.com/overwatch/popularnewreleases")
     
-    # Should return a list of the games on the first page of the 'Popular New Releases' on Steam's website as a collection of instances of the Game class.
+   # [game_1, game_2]
+   end
     
-    game_1 = self.new("Skyrim", "96% positive out of 820 reviews.", "steam.com/skyrim/popularnewreleases")
-    game_2 = self.new("Overwatch", "98% positive out of 1,162 reviews.", "steam.com/overwatch/popularnewreleases")
+   def self.scrape_games
+     #Scrape the first page of the 'Popular New Releases' from Steam's website. Store these games as instances of the Game class in the 'games' array.
+    games = []
+      
+    doc = Nokogiri::HTML(open("https://store.steampowered.com/search/?sort_by=Released_DESC&os=win&filter=popularnew"))
     
-    [game_1, game_2]
+    #Must obtain the name of each game. Can be done by extracting the inner text from each hyperlink under the ID of "search results container."
     
+    game_name = doc.search("#search_result_container span.title").text 
+    binding.pry 
     
+    #Must obtain the % of positive reviews for each game. Can be done by extracting the inner text of the 'span' under the class 'col search_reviewscore responsive_secondrow' under each game's respective hyperlink.
+    game_review = doc.search()
+      
+    games
    end 
   
   

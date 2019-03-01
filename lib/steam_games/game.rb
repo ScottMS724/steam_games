fc27468b14dbcb74eae0050e2f5412a4ea0ad12b
Scott Silverman <scottms724@gmail.com>
2019-03-01 20:17:24 +0000

    WIP: #scrape_games method

--- lib/steam_games/game.rb
+++ lib/steam_games/game.rb
@@ -1,11 +1,10 @@
 class SteamGames::Game 
-  attr_accessor :name, :review, :url 
+  attr_accessor :name, :date_released, :url 
   
-   def initialize(name, review, url)
-      @name = name 
-      @review = review 
-      @url = url 
-   end 
+  def initialize(name, date_released)
+    @name = name
+    @date_released = date_released 
+  end 
   
    def self.popular_new_releases
      self.scrape_games
@@ -18,20 +17,23 @@ class SteamGames::Game
     
    def self.scrape_games
      #Scrape the first page of the 'Popular New Releases' from Steam's website. Store these games as instances of the Game class in the 'games' array.
-    games = []
+    games_hash = []
       
     doc = Nokogiri::HTML(open("https://store.steampowered.com/search/?sort_by=Released_DESC&os=win&filter=popularnew"))
+    games = doc.search("div.responsive_search_name_combined")
+    games.each do |game|
+      name = game.search("span.title").text 
+      date_released = game.search("div.col.search_released.responsive_secondrow").text 
+      game = self.new(name, date_released)
+      games_hash << game 
+      binding.pry
+    end 
     
-    #Must obtain the name of each game. Can be done by extracting the inner text from each hyperlink under the ID of "search results container."
     
-    game_name = doc.search("#search_result_container span.title").text 
-    binding.pry 
     
-    #Must obtain the % of positive reviews for each game. Can be done by extracting the inner text of the 'span' under the class 'col search_reviewscore responsive_secondrow' under each game's respective hyperlink.
-    game_review = doc.search()
-      
-    games
+    #Must obtain the element that contains all of the data for each game on Steam's website, make each game its own object, and assign the data to its approrpiate game object.
+    
+    games_hash
    end 
-  
   
 end 

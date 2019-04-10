85d3653baf3d2ac38c6ca5e65d3c12e9f8aba9ec
Scott Silverman <scottms724@gmail.com>
2019-04-10 19:00:09 +0000

    Final draft.

--- lib/steam_games/game.rb
+++ lib/steam_games/game.rb
@@ -3,10 +3,10 @@ class SteamGames::Game
   
   @@all = [] 
   
-  def initialize(name, date_released, review)
+  def initialize(name, date_released, url)
     @name = name
     @date_released = date_released 
-    @review = review 
+    @url = url 
     @@all << self 
   end 
   
@@ -14,31 +14,12 @@ class SteamGames::Game
     @@all 
   end 
   
+  def review
+    @review ||= SteamGames::Scraper.scrape_review(url)
+  end 
+  
   def self.find_by_name(name)
     SteamGames::Game.all.detect { |the_game| the_game.name.downcase == name.downcase } 
   end 
-  
-  
-  #def self.popular_new_releases
-  #  self.scrape_games
-  #end
-    
-#  def self.scrape_games
-#    counter = 0 
-#    
-#    doc = Nokogiri::HTML(open("https://store.steampowered.com/search/?sort_by=Released_DESC&os=win&filter=popularnew"))
-#    all_games = doc.search("div.responsive_search_name_combined")
-#    all_games.each do |game|
-#      name = game.search("span.title").text 
-#      date_released = game.search("div.col.search_released.responsive_secondrow").text 
-#        specific_game_url = doc.css("div#search_result_container a.search_result_row")[counter].attr("href")
-#        specific_game_url_parse = Nokogiri::HTML(open(specific_game_url))
-#      review = specific_game_url_parse.search("div.user_reviews_summary_row").text.strip 
-#      game = self.new(name, date_released, review)
-#      
-#      counter += 1 
-#      
-#    end 
-#  end 
   
 end 
--- lib/steam_games/scraper.rb
+++ lib/steam_games/scraper.rb
@@ -1,41 +1,31 @@
 class SteamGames::Scraper 
   
-#  def self.scrape_games
-#    counter = 0 
-#    
-#    doc = Nokogiri::HTML(open("https://store.steampowered.com/search/?sort_by=Released_DESC&os=win&filter=popularnew"))
-#    all_games = doc.search("div.responsive_search_name_combined")
-#    all_games.each do |game|
-#      name = game.search("span.title").text 
-#      date_released = game.search("div.col.search_released.responsive_secondrow").text 
-#        specific_game_url = doc.css("div#search_result_container a.search_result_row")[counter].attr("href")
-#        specific_game_url_parse = Nokogiri::HTML(open(specific_game_url))
-#      review = specific_game_url_parse.search("div.user_reviews_summary_row").text.strip 
-#      game = self.new(name, date_released, review)
-#      
-#    counter += 1 
-#      
-#  end 
-  
- # def self.retrieve_page 
- #   Nokogiri::HTML(open("https://store.steampowered.com/search/?sort_by=Released_DESC&os=win&filter=popularnew"))
- #  end 
-  
   def self.create_game_objects
-    counter = 0 
     
     doc = Nokogiri::HTML(open("https://store.steampowered.com/search/?sort_by=Released_DESC&os=win&filter=popularnew"))
     all_games = doc.search("div.responsive_search_name_combined")
-    all_games.each do |game|
+    all_games.each_with_index do |game, counter|
       name = game.search("span.title").text 
       date_released = game.search("div.col.search_released.responsive_secondrow").text 
-        specific_game_url = doc.css("div#search_result_container a.search_result_row")[counter].attr("href")
-        specific_game_url_parse = Nokogiri::HTML(open(specific_game_url))
-      review = specific_game_url_parse.search("div.user_reviews_summary_row").text.strip 
-      game = SteamGames::Game.new(name, date_released, review)
+      specific_game_url = doc.css("div#search_result_container a.search_result_row")[counter].attr("href")
+      #specific_game_url_parse = Nokogiri::HTML(open(specific_game_url))
+      #review = specific_game_url_parse.search("div.user_reviews_summary_row").text.strip 
+      game = SteamGames::Game.new(name, date_released, specific_game_url)
       
-    counter += 1 
     end 
   end 
   
+  def self.scrape_review(specific_game_url)
+    puts "Scraping #{specific_game_url}"
+    #specific_game_url = doc.css("div#search_result_container a.search_result_row")[counter].attr("href")
+    specific_game_url_parse = Nokogiri::HTML(open(specific_game_url))
+    specific_game_url_parse.search("div.user_reviews_summary_row").text.strip 
+  end 
+  
 end 
+
+# add another class method, `self.scrape_review(url)` ... this method should only be called when we request the details of a specific game 
+# you will also have to adjust your setter in your Game class for the review... 
+# You might also have to adjust your initialize method in the Game class..
+# In your custom setter, use memoization to prevent scraping for the same review more than once
+
